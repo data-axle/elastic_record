8e14881efaebaea9d8cbba941ad1901e3ea3ad29
Matthew Higgins <developer@matthewhiggins.com>
2012-11-18 10:14:17 -0800

    add some shoddy tests for controller_runtime

--- lib/elastic_record/railties/controller_runtime.rb
+++ lib/elastic_record/railties/controller_runtime.rb
@@ -6,31 +6,6 @@ module ElasticRecord
     module ControllerRuntime #:nodoc:
       extend ActiveSupport::Concern
 
-    protected
-
-      attr_internal :elastic_record_runtime
-
-      def process_action(action, *args)
-        # We also need to reset the runtime before each action
-        # because of queries in middleware or in cases we are streaming
-        # and it won't be cleaned up by the method below.
-        ElasticRecord::LogSubscriber.reset_runtime
-        super
-      end
-
-      def cleanup_view_runtime
-        runtime_before_render = ElasticRecord::LogSubscriber.reset_runtime
-        runtime = super
-        runtime_after_render = ElasticRecord::LogSubscriber.reset_runtime
-        self.elastic_record_runtime = runtime_before_render + runtime_after_render
-        runtime - runtime_after_render
-      end
-
-      def append_info_to_payload(payload)
-        super
-        payload[:elastic_record_runtime] = (elastic_record_runtime || 0) + ElasticRecord::LogSubscriber.reset_runtime
-      end
-
       module ClassMethods # :nodoc:
         def log_process_action(payload)
           messages, elastic_record_runtime = super, payload[:elastic_record_runtime]
@@ -40,6 +15,31 @@ module ElasticRecord
           messages
         end
       end
+
+      #private
+
+        attr_internal :elastic_record_runtime
+
+        def process_action(action, *args)
+          # We also need to reset the runtime before each action
+          # because of queries in middleware or in cases we are streaming
+          # and it won't be cleaned up by the method below.
+          ElasticRecord::LogSubscriber.reset_runtime
+          super
+        end
+
+        def cleanup_view_runtime
+          runtime_before_render = ElasticRecord::LogSubscriber.reset_runtime
+          runtime = super
+          runtime_after_render = ElasticRecord::LogSubscriber.reset_runtime
+          self.elastic_record_runtime = runtime_before_render + runtime_after_render
+          runtime - runtime_after_render
+        end
+
+        def append_info_to_payload(payload)
+          super
+          payload[:elastic_record_runtime] = (elastic_record_runtime || 0) + ElasticRecord::LogSubscriber.reset_runtime
+        end
     end
   end
 end
--- test/elastic_record/railties/controller_runtime_test.rb
+++ test/elastic_record/railties/controller_runtime_test.rb
@@ -8,17 +8,42 @@ class ElasticRecord::Railties::ControllerRuntimeTest < MiniTest::Spec
     end
 
     def cleanup_view_runtime
-      yield
+      12
     end
 
     def append_info_to_payload(payload)
       payload[:foo] = 42
     end
+
   end
 
-  TestRuntime.include ElasticRecord::Railties::ControllerRuntime
+  class ElasticRuntime < TestRuntime
+    include ElasticRecord::Railties::ControllerRuntime
+  end
 
-  def test_stuff
-    
+  def test_cleanup_view_runtime
+    runtime = ElasticRuntime.new
+    ElasticRecord::LogSubscriber.runtime = 10
+
+    runtime.cleanup_view_runtime
+
+    assert_equal 0, ElasticRecord::LogSubscriber.runtime
+  end
+
+  def test_append_info_to_payload
+    runtime = ElasticRuntime.new
+    payload = {}
+    runtime.append_info_to_payload(payload)
+
+    assert_equal 42, payload[:foo]
+    assert payload.key?(:elastic_record_runtime)
+  end
+
+  def test_log_process_action
+    payload = {elastic_record_runtime: 12.3}
+    messages = ElasticRuntime.log_process_action(payload)
+
+    assert_equal 2, messages.size
+    assert_equal "ElasticRecord: 12.3ms", messages.last
   end
 end
