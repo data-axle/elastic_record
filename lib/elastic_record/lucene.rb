207da55d429dcedb99482dba26818b8ba1caf28a
Matthew Higgins <developer@matthewhiggins.com>
2013-01-23 18:06:54 -0800

    Better documentation and only support block for phrase_match

--- lib/elastic_record/lucene.rb
+++ lib/elastic_record/lucene.rb
@@ -11,33 +11,31 @@ module ElasticRecord
         query.gsub(ESCAPE_REGEX, "\\\\\\1")
       end
 
-      def query_words(query)
-        # If we have an odd number of double quotes,
-        # add a double quote to the end so that shellwords
-        # does not crap out.
-        if query.count('"') % 2 == 1
-          query = "#{query}\""
-        end
-
-        Shellwords::shellwords query.gsub("'", "\"'\"")
-      end
-
       # Returns a lucene query that works like GMail
-      def smart_query(query, fields, &block)
+      def match_phrase(query, fields, &block)
         return if query.blank?
 
-        words = query_words(query)
+        words = split_phrase_into_words(query)
 
         words.map do |word|
           if word =~ /^(\w+):(.+)$/ && fields.include?($1)
-            match_word $2, [$1], &block
+            match_word $2, [block_given? ? yield($1) : $1]
           else
-            match_word word, fields, &block
+            match_word word, (block_given? ? fields.map(&block) : fields)
           end
         end.join(' AND ')
       end
 
-      def match_word(word, fields, &block)
+      # Performs a prefix match on the word:
+      # 
+      # ElasticRecord::Lucene.match_word('blue', ['color', 'name'])
+      #   => (color:blue* OR name:blue*)
+      # 
+      # In the case that the word has special characters, it is wrapped in quotes:
+      # 
+      # ElasticRecord::Lucene.match_word('A&M', ['name'])
+      #   => (name:"A&M")
+      def match_word(word, fields)
         if word =~ / / || word =~ ESCAPE_REGEX
           word = "\"#{word.gsub('"', '')}\""
         else
@@ -45,12 +43,27 @@ module ElasticRecord
         end
 
         or_query = fields.map do |field|
-          field = yield(field) if block_given?
           "#{field}:#{word}"
         end.join(' OR ')
 
         "(#{or_query})"
       end
+
+      private
+        # Converts a sentence into the words:
+        # 
+        # split_phrase_into_words('his "blue fox"')
+        # => ['his', 'blue fox']
+        def split_phrase_into_words(phrase)
+          # If we have an odd number of double quotes,
+          # add a double quote to the end so that shellwords
+          # does not crap out.
+          if phrase.count('"') % 2 == 1
+            phrase = "#{phrase}\""
+          end
+
+          Shellwords::shellwords phrase.gsub("'", "\"'\"")
+        end
     end
   end
 end
