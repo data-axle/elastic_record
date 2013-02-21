dcaa4a481801c0b954e6bf42f7bfb1b833750739
Matthew Higgins <developer@matthewhiggins.com>
2013-02-21 11:57:11 -0800

    Add a test for percolator deletion

--- test/elastic_record/index/percolator_test.rb
+++ test/elastic_record/index/percolator_test.rb
@@ -1,23 +1,25 @@
 require 'helper'
 
 class ElasticRecord::Index::PercolatorTest < MiniTest::Spec
-  def setup
-    super
-    index.disable_deferring!
-  end
-
   def test_create_percolator
-    index.delete(index.percolator_index_name) if index.exists?(index.percolator_index_name)
+    index.delete_percolator('green') if index.percolator_exists?('green')
+    index.delete_percolator('blue') if index.percolator_exists?('blue')
 
     index.create_percolator('green', 'color' => 'green')
 
-    assert index.exists?(index.percolator_index_name)
     assert index.percolator_exists?('green')
     refute index.percolator_exists?('blue')
   end
 
-  def test_percolator_index_name
-    assert_equal 'percolate_widgets', index.percolator_index_name
+  def test_delete_percolator
+    index.create_percolator('green', 'color' => 'green')
+    assert index.percolator_exists?('green')
+
+    index.delete_percolator('green')
+    refute index.percolator_exists?('green')
+  end
+
+  def test_percolate
   end
 
   private
