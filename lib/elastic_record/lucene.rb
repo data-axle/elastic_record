f61f20c5a74340a6217a218b1a09703011535b2c
Matthew Higgins <developer@matthewhiggins.com>
2013-05-20 20:35:54 -0700

    remove complex query parsing

--- lib/elastic_record/lucene.rb
+++ lib/elastic_record/lucene.rb
@@ -10,60 +10,6 @@ module ElasticRecord
       def escape(query)
         query.gsub(ESCAPE_REGEX, "\\\\\\1")
       end
-
-      # Returns a lucene query that works like GMail
-      def match_phrase(query, fields, &block)
-        return if query.blank?
-
-        words = split_phrase_into_words(query)
-
-        words.map do |word|
-          if word =~ /^(\w+):(.+)$/ && fields.include?($1)
-            match_word $2, [block_given? ? yield($1) : $1]
-          else
-            match_word word, (block_given? ? fields.map(&block) : fields)
-          end
-        end.join(' AND ')
-      end
-
-      # Performs a prefix match on the word:
-      # 
-      # ElasticRecord::Lucene.match_word('blue', ['color', 'name'])
-      #   => (color:blue* OR name:blue*)
-      # 
-      # In the case that the word has special characters, it is wrapped in quotes:
-      # 
-      # ElasticRecord::Lucene.match_word('A&M', ['name'])
-      #   => (name:"A&M")
-      def match_word(word, fields)
-        if word =~ / / || word =~ ESCAPE_REGEX
-          word = "\"#{word.gsub('"', '')}\""
-        else
-          word = "#{word}*"
-        end
-
-        or_query = fields.map do |field|
-          "#{field}:#{word}"
-        end.join(' OR ')
-
-        "(#{or_query})"
-      end
-
-      private
-        # Converts a sentence into the words:
-        # 
-        # split_phrase_into_words('his "blue fox"')
-        # => ['his', 'blue fox']
-        def split_phrase_into_words(phrase)
-          # If we have an odd number of double quotes,
-          # add a double quote to the end so that shellwords
-          # does not crap out.
-          if phrase.count('"') % 2 == 1
-            phrase = "#{phrase}\""
-          end
-
-          Shellwords::shellwords phrase.gsub("'", "\"'\"")
-        end
     end
   end
 end
--- test/elastic_record/lucene_test.rb
+++ test/elastic_record/lucene_test.rb
@@ -5,30 +5,4 @@ class ElasticRecord::LuceneTest < MiniTest::Spec
     assert_equal "\\\\", ElasticRecord::Lucene.escape("\\")
     assert_equal "Matt \\&& Joe", ElasticRecord::Lucene.escape("Matt && Joe")
   end
-
-  def test_match_phrase
-    assert_match_phrase nil, '', ['name']
-    assert_match_phrase nil, nil, ['name']
-
-    assert_match_phrase '(name:foo*)', 'foo', ['name']
-    assert_match_phrase '(name:"foo-bar")', 'foo-bar', ['name']
-    assert_match_phrase "(name:bob's*)", "bob's", ['name']
-    assert_match_phrase '(name:foo* OR street:foo*)', 'foo', ['name', 'street']
-    assert_match_phrase '(name:"foo bar" OR street:"foo bar") AND (name:faz* OR street:faz*)', '"foo bar" faz', ['name', 'street']
-    assert_match_phrase '(street:"42 place") AND (name:bar*)', 'street:"42 place" name:bar', ['name', 'street']
-  end
-
-  def test_match_phrase_with_unmatched_quotes
-    assert_match_phrase '(name:"foo bar")', '"foo bar', ['name']
-  end
-
-  def test_match_phrase_with_block
-    assert_match_phrase '(name.analyzed:foo*)', 'foo', ['name'] { |f| "#{f}.analyzed" }
-  end
-
-  private
-
-    def assert_match_phrase(expected, query, fields, &block)
-      assert_equal expected, ElasticRecord::Lucene.match_phrase(query, fields, &block)
-    end
 end
