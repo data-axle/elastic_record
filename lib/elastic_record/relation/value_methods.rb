f7fe9a9047b2bdf206e4a0c8c59f00f126781def
Mike Mondragon <mikemondragon@gmail.com>
2013-06-05 12:17:39 -0700

    Do not conflate inlcudes and eager_load from ActiveRecord.

--- lib/elastic_record/relation/value_methods.rb
+++ lib/elastic_record/relation/value_methods.rb
@@ -1,6 +1,6 @@
 module ElasticRecord
   class Relation
-    MULTI_VALUE_METHODS  = [:extending, :facet, :filter, :order, :select, :includes]
+    MULTI_VALUE_METHODS  = [:extending, :facet, :filter, :order, :select, :eager_load]
     SINGLE_VALUE_METHODS = [:query, :limit, :offset, :reverse_order]
   end
 end
