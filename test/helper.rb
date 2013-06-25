8e8ec986f3e79e9994bfcedf9f5559e0e257321b
Matthew Higgins <developer@matthewhiggins.com>
2013-06-25 10:16:26 -0700

    Package query_counter into its own file. @monde

--- test/helper.rb
+++ test/helper.rb
@@ -4,6 +4,7 @@ Bundler.require
 require 'minitest/autorun'
 
 require 'support/connect'
+require 'support/query_counter'
 require 'support/models/test_model'
 require 'support/models/warehouse'
 require 'support/models/widget'
@@ -35,56 +36,5 @@ module MiniTest
         model.elastic_index.reset_deferring!
       end
     end
-
-    def assert_queries(num = 1, options = {})
-      ignore_none = options.fetch(:ignore_none) { num == :any }
-      ElasticSearchCounter.clear_log
-      x = yield
-      the_log = ignore_none ? ElasticSearchCounter.log_all : ElasticSearchCounter.log
-      if num == :any
-        assert_operator the_log.size, :>=, 1, "1 or more queries expected, but none were executed."
-      else
-        queries = the_log.map { |event| "  #{event[:request].method} #{event[:request].path} #{event[:request].body}\n" }.join("\n")
-
-        mesg = "#{the_log.size} instead of #{num} queries were executed.#{the_log.size == 0 ? '' : "\nQueries:\n#{queries}"}"
-        assert_equal num, the_log.size, mesg
-      end
-
-      x
-    end
-
-    def assert_no_queries(&block)
-      assert_queries(0, :ignore_none => true, &block)
-    end
   end
-
-  class ElasticSearchCounter
-    class << self
-      attr_accessor :ignored_elastic, :log, :log_all
-      def clear_log; self.log = []; self.log_all = []; end
-    end
-
-    self.clear_log
-
-    self.ignored_elastic = []
-
-    elastic_search_ignored = []
-
-    [elastic_search_ignored].each do |db_ignored_elastic|
-      ignored_elastic.concat db_ignored_elastic
-    end
-
-    attr_reader :ignore
-
-    def initialize(ignore = Regexp.union(self.class.ignored_elastic))
-      @ignore = ignore
-    end
-
-    def call(name, start, finish, message_id, values)
-      self.class.log_all << values
-      self.class.log << values unless ignore =~ values[:request].path
-    end
-  end
-
-  ActiveSupport::Notifications.subscribe('request.elastic_record', ElasticSearchCounter.new)
 end
