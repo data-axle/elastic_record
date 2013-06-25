b8231f0698f4264cb096794f9fb7c7f782a04970
Matthew Higgins <developer@matthewhiggins.com>
2013-06-25 10:12:17 -0700

    run integration tests along with normal tests

--- lib/elastic_record/integration/active_record.rb
+++ lib/elastic_record/integration/active_record.rb
@@ -1,7 +1,7 @@
 module ActiveRecord
   class Base
     def self.load_elastic_record_hits(ids)
-      order("FIELD(#{connection.quote_column_name(primary_key)}, #{ids.join(',')})").where(id: ids)
+      order("FIELD(#{connection.quote_column_name(primary_key)}, #{ids.join(',')})").find(ids)
     end
   end
 end
