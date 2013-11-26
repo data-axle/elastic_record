a3eef2821dd1f54a0ddac5fa82a55f32a021d97e
Matthew Higgins <developer@matthewhiggins.com>
2013-11-26 09:51:44 -0800

    Add a test for the update callback

--- test/elastic_record/integration/active_record_test.rb
+++ test/elastic_record/integration/active_record_test.rb
@@ -38,6 +38,15 @@ module ElasticRecord
       assert_equal [bear_product, poo_product], Project.elastic_relation.order(name: 'asc')
       assert_equal [poo_product, bear_product], Project.elastic_relation.order(name: 'desc')
     end
+
+    def test_update_callback
+      project = Project.create! name: "Ideas"
+      Project.elastic_index.refresh
+      project.update! name: 'Terrible Stuff'
+      Project.elastic_index.refresh
+
+      assert_equal [project], Project.elastic_relation.filter(name: 'Terrible Stuff')
+    end
   end
 end
 
