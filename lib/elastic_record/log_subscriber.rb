db9e318121581cce8a272992a6c726e76a946c2b
AlexanderPavlenko <alerticus@gmail.com>
2014-09-09 14:46:50 +0400

    crop long payloads e.g. attachments in base64

--- lib/elastic_record/log_subscriber.rb
+++ lib/elastic_record/log_subscriber.rb
@@ -19,8 +19,13 @@ module ElasticRecord
       payload = event.payload
       request_log = "#{payload[:request].method} #{payload[:http].address}:#{payload[:http].port}#{payload[:request].path}"
 
-      if payload[:request].body
-        request_log << " '#{payload[:request].body}'"
+      if (payload_body = payload[:request].body)
+        request_log <<
+          if payload_body.size > 420
+            " '#{payload_body[0...200]}[CUT #{payload_body.size - 400} chars]#{payload_body[-200..-1]}'"
+          else
+            " '#{payload_body}'"
+          end
       end
 
       debug "(#{event.duration.round(1)}ms) #{request_log}"
