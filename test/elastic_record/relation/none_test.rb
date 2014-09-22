b51dfba5f8312db906b1e86093540c412ab1b96e
Matthew Higgins <developer@matthewhiggins.com>
2014-09-22 10:38:04 -0700

    Add test for None#aggregationsg

--- test/elastic_record/relation/none_test.rb
+++ test/elastic_record/relation/none_test.rb
@@ -7,6 +7,7 @@ class ElasticRecord::Relation::NoneTest < MiniTest::Test
     assert_equal 0,     none.size
     assert_equal 0,     none.count
     assert_equal [],    none.to_a
+    assert_equal({},    none.aggregations)
 
     expected_elastic = {"not" => {"match_all" => {}}}
     assert_equal expected_elastic, none.as_elastic
