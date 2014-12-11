de215cf71e1bd4a4e4bbc55c4e6d53955c07d34a
Eric Barendt <ericb@gotime.com>
2014-12-11 13:05:57 -0800

    fix test

--- test/elastic_record/model_test.rb
+++ test/elastic_record/model_test.rb
@@ -7,7 +7,7 @@ class ElasticRecord::ModelTest < MiniTest::Test
   def test_elastic_connection
     connection = Widget.elastic_connection
 
-    assert_equal [ElasticRecord::Config.servers], connection.servers
+    assert_equal ElasticRecord::Config.servers, connection.servers
     assert_equal ElasticRecord::Config.connection_options, connection.options
   end
 
