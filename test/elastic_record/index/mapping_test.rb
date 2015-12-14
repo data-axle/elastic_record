5cba5487354f5e1b0513ddc7ce6753c4f7ff9bad
Mark Lee <Mark.Lee@infogroup.com>
2015-12-14 10:36:39 -0800

    Hardcode unique index name rather than use a random one

--- test/elastic_record/index/mapping_test.rb
+++ test/elastic_record/index/mapping_test.rb
@@ -2,12 +2,14 @@ require 'helper'
 
 class ElasticRecord::Index::MappingTest < MiniTest::Test
   def test_delete_mapping
-    index_name = index.create("widgets_#{SecureRandom.random_number(100_000_000_000_000)}")
+    index_name = index.create("widgets_test_delete_mapping")
     refute_nil index.get_mapping(index_name)['widget']
 
     index.delete_mapping(index_name)
 
     assert_nil index.get_mapping(index_name)
+  ensure
+    index.delete("widgets_test_delete_mapping")
   end
 
   def test_default_mapping
