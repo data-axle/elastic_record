66e7f503cb2388ea28681c233920023b505111d2
Mark Lee <Mark.Lee@infogroup.com>
2016-04-06 12:23:40 -0700

    Replace fakeweb with webmock

--- test/helper.rb
+++ test/helper.rb
@@ -3,13 +3,13 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
 
 require 'minitest/autorun'
-
-FakeWeb.allow_net_connect = %r[^https?://127.0.0.1]
+require 'webmock/minitest'
 
 module MiniTest
   class Test
     def setup
-      FakeWeb.clean_registry
+      WebMock.reset!
+      WebMock.disable_net_connect!(allow_localhost: true)
 
       ElasticRecord::Config.models.each do |model|
         model.elastic_index.enable_deferring!
