a67637c1ee3ed832294e8bacf9b83209678d1c49
Mark Lee <Mark.Lee@infogroup.com>
2016-04-11 10:15:26 -0700

    Move disable_net_connect outside of setup

--- test/helper.rb
+++ test/helper.rb
@@ -1,15 +1,16 @@
 ENV["RAILS_ENV"] = "test"
 
-require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
+require File.expand_path("../../test/dummy/config/environment.rb", __FILE__)
 
 require 'minitest/autorun'
 require 'webmock/minitest'
+
+WebMock.disable_net_connect!(allow_localhost: true)
 
 module MiniTest
   class Test
     def setup
       WebMock.reset!
-      WebMock.disable_net_connect!(allow_localhost: true)
 
       ElasticRecord::Config.models.each do |model|
         model.elastic_index.enable_deferring!
