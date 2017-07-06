85f69263e75eceab2825de51e3a11fb2fa53a520
Matthew Higgins <developer@matthewhiggins.com>
2017-07-05 18:02:51 -0700

    Switch off multi_field

--- test/dummy/app/models/widget.rb
+++ test/dummy/app/models/widget.rb
@@ -9,7 +9,7 @@ class Widget
 
   self.elastic_index.mapping[:properties].update(
     'name' => {
-      type: 'multi_field',
+      type: 'string',
       fields: {
         name: {type: 'string', index: 'not_analyzed'},
         analyzed: {type: 'string', index: 'analyzed'}
