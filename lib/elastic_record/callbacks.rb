7e883b2b48503371e8a8afcfd568e4bdc9d93116
Dan Cromer <cromer.dan@gmail.com>
2017-08-23 16:40:49 -0700

    Name the callbacks

--- lib/elastic_record/callbacks.rb
+++ lib/elastic_record/callbacks.rb
@@ -4,19 +4,26 @@ module ElasticRecord
       return unless base.respond_to?(:after_save) &&  base.respond_to?(:after_destroy)
 
       base.class_eval do
-        after_create do
-          self.class.elastic_index.index_record self
-        end
-
-        after_update if: :changed? do
-          method = self.class.elastic_index.partial_updates ? :update_record : :index_record
-          self.class.elastic_index.send(method, self)
-        end
-
-        after_destroy do
-          self.class.elastic_index.delete_document id
-        end
+        after_create :create_index_document
+        after_update :update_index_document, if: :changed?
+        after_destroy :delete_index_document
       end
     end
+
+    private
+
+      def create_index_document
+        self.class.elastic_index.index_record self
+      end
+
+      def update_index_document
+        method = self.class.elastic_index.partial_updates ? :update_record : :index_record
+
+        self.class.elastic_index.send(method, self)
+      end
+
+      def delete_index_document
+        self.class.elastic_index.delete_document id
+      end
   end
 end
