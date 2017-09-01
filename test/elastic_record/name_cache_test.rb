60cc4de92007b3fa39d176c452d0f7ee19f00ffe
Eric Barendt <ericb@gotime.com>
2017-09-01 11:20:56 -0700

    Remove the index after running

--- test/elastic_record/name_cache_test.rb
+++ test/elastic_record/name_cache_test.rb
@@ -6,7 +6,11 @@ class ElasticRecord::NameCacheTest < MiniTest::Test
   end
 
   def test_index_name
-    Felon.elastic_index.create_and_deploy("felons_datestamp")
-    assert_equal "felons_datestamp", Felon.current_index_name
+    begin
+      Felon.elastic_index.create_and_deploy("felons_datestamp")
+      assert_equal "felons_datestamp", Felon.current_index_name
+    ensure
+      Felon.elastic_index.delete("felons_datestamp")
+    end
   end
 end
