098ad754b1e4211eabcfa997a21ccd4a0130e446
Patrick Schmitz <patrick.schmitz@infogroup.com>
2017-09-13 14:52:14 -0700

    Better errors

--- lib/elastic_record/index/documents.rb
+++ lib/elastic_record/index/documents.rb
@@ -93,7 +93,7 @@ module ElasticRecord
       end
 
       def update_document(id, document, doctype: model.doctype, parent: nil, index_name: alias_name)
-        raise "Cannot update a document with empty id" if id.blank?
+        raise ElasticRecord::Error, "Cannot update a document with empty id" if id.blank?
         params = {doc: document, doc_as_upsert: true}
 
         if batch = current_bulk_batch
@@ -111,7 +111,7 @@ module ElasticRecord
       end
 
       def delete_document(id, doctype: model.doctype, parent: nil, index_name: alias_name)
-        raise "Cannot delete document with empty id" if id.blank?
+        raise ElasticRecord::Error, "Cannot delete document with empty id" if id.blank?
         index_name ||= alias_name
 
         if batch = current_bulk_batch
