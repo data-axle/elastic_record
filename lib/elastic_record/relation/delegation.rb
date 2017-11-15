3bf0cbad285cb7b77fe57a242baff97e7e7c8bcf
Matthew Higgins <developer@matthewhiggins.com>
2017-11-15 13:37:35 -0800

    Support this

--- lib/elastic_record/relation/delegation.rb
+++ lib/elastic_record/relation/delegation.rb
@@ -10,6 +10,10 @@ module ElasticRecord
       end
 
       private
+        def respond_to_missing?(method, include_private = false)
+          super || klass.respond_to?(method, include_private) || Array.method_defined?(method, include_private)
+        end
+
         def method_missing(method, *args, &block)
           if klass.respond_to?(method)
             scoping { klass.send(method, *args, &block) }
