7eb03219f7072a2a808dd2231561b601e3e08528
Matthew Higgins <developer@matthewhiggins.com>
2018-01-08 16:30:50 -0800

    Add new concern, BulkDocuments

--- lib/elastic_record/index/documents.rb
+++ lib/elastic_record/index/documents.rb
@@ -75,21 +75,13 @@ module ElasticRecord
       end
 
       def index_document(id, document, doctype: model.doctype, parent: nil, index_name: alias_name)
-        if batch = current_bulk_batch
-          instructions = { _index: index_name, _type: doctype.name, _id: id }
-          instructions[:parent] = parent if parent
+        path = "/#{index_name}/#{doctype.name}/#{id}"
+        path << "?parent=#{parent}" if parent
 
-          batch << { index: instructions }
-          batch << document
+        if id
+          connection.json_put path, document
         else
-          path = "/#{index_name}/#{doctype.name}/#{id}"
-          path << "?parent=#{parent}" if parent
-
-          if id
-            connection.json_put path, document
-          else
-            connection.json_post path, document
-          end
+          connection.json_post path, document
         end
       end
 
@@ -97,34 +89,19 @@ module ElasticRecord
         raise "Cannot update a document with empty id" if id.blank?
         params = {doc: document, doc_as_upsert: true}
 
-        if batch = current_bulk_batch
-          instructions = { _index: index_name, _type: doctype.name, _id: id, _retry_on_conflict: 3 }
-          instructions[:parent] = parent if parent
-
-          batch << { update: instructions }
-          batch << params
-        else
-          path = "/#{index_name}/#{doctype.name}/#{id}/_update?retry_on_conflict=3"
-          path << "&parent=#{parent}" if parent
-
-          connection.json_post path, params
-        end
+        path = "/#{index_name}/#{doctype.name}/#{id}/_update?retry_on_conflict=3"
+        path << "&parent=#{parent}" if parent
+        connection.json_post path, params
       end
 
       def delete_document(id, doctype: model.doctype, parent: nil, index_name: alias_name)
         raise "Cannot delete document with empty id" if id.blank?
         index_name ||= alias_name
 
-        if batch = current_bulk_batch
-          instructions = { _index: index_name, _type: doctype.name, _id: id, _retry_on_conflict: 3 }
-          instructions[:parent] = parent if parent
-          batch << { delete: instructions }
-        else
-          path = "/#{index_name}/#{doctype.name}/#{id}"
-          path << "&parent=#{parent}" if parent
+        path = "/#{index_name}/#{doctype.name}/#{id}"
+        path << "&parent=#{parent}" if parent
 
-          connection.json_delete path
-        end
+        connection.json_delete path
       end
 
       def delete_by_query(query)
@@ -163,43 +140,6 @@ module ElasticRecord
         connection.json_get("/_search/scroll?#{options.to_query}")
       end
 
-      def bulk(options = {})
-        connection.bulk_stack.push []
-
-        yield
-
-        if current_bulk_batch.any?
-          body = current_bulk_batch.map { |action| "#{JSON.generate(action)}\n" }.join
-          results = connection.json_post("/_bulk?#{options.to_query}", body)
-          verify_bulk_results(results)
-        end
-      ensure
-        connection.bulk_stack.pop
-      end
-
-      def bulk_add(batch, index_name: alias_name)
-        bulk do
-          batch.each do |record|
-            index_record(record, index_name: index_name)
-          end
-        end
-      end
-
-      def current_bulk_batch
-        connection.bulk_stack.last
-      end
-
-      private
-
-        def verify_bulk_results(results)
-          return unless results.is_a?(Hash)
-
-          errors = results['items'].select do |item|
-            item.values.first['error']
-          end
-
-          raise ElasticRecord::BulkError.new(errors) unless errors.empty?
-        end
     end
   end
 end
---
+++ test/elastic_record/index/bulk_documents_test.rb
@@ -0,0 +1,79 @@
+require 'helper'
+
+class ElasticRecord::Index::BulkDocumentsTest < MiniTest::Test
+  class InheritedWidget < Widget
+    def self.base_class
+      Widget
+    end
+  end
+
+  def test_bulk_add
+    record = Widget.new(id: 'abc', color: 'red')
+
+    index.bulk_add [record]
+
+    assert index.record_exists?('abc')
+    refute index.record_exists?('xyz')
+  end
+
+  def test_bulk
+    assert_nil index.instance_variable_get(:@_batch)
+
+    index.bulk do
+      index.index_document '5', color: 'green'
+      index.update_document '5', color: 'blue'
+      index.delete_document '3'
+
+      expected = [
+        {index: {_index: index.alias_name, _type: "widget", _id: "5"}},
+        {color: "green"},
+        {update: {_index: "widgets", _type: "widget", _id: "5", _retry_on_conflict: 3}},
+        {doc: {color: "blue"}, doc_as_upsert: true},
+        {delete: {_index: index.alias_name, _type: "widget", _id: "3", _retry_on_conflict: 3}}
+      ]
+      assert_equal expected, index.current_bulk_batch
+    end
+
+    assert_nil index.current_bulk_batch
+  end
+
+  def test_bulk_error
+    without_deferring(index) do
+      begin
+        index.bulk do
+          index.index_document '5', color: 'green'
+          index.index_document '3', color: {'bad' => 'stuff'}
+        end
+        refute index.record_exists?('3')
+        assert false, 'Expected ElasticRecord::BulkError'
+      rescue => e
+        assert_match '[{"index"', e.message
+      end
+    end
+  end
+
+  def test_bulk_inheritence
+    without_deferring(index) do
+      index.bulk do
+        InheritedWidget.elastic_index.index_document '5', color: 'green'
+
+        expected = [
+          {index: {_index: index.alias_name, _type: "widget", _id: "5"}},
+          {color: "green"}
+        ]
+        assert_equal expected, index.current_bulk_batch
+      end
+    end
+  end
+
+  def without_deferring(index)
+    index.disable_deferring!
+    yield
+    index.reset
+    index.enable_deferring!
+  end
+
+  def index
+    @index ||= Widget.elastic_index
+  end
+end
