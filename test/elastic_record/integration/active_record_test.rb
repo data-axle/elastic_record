2f2f7807f3df58d680c89a578accd78762552aaf
Matthew Higgins <developer@matthewhiggins.com>
2018-02-16 08:21:08 -0600

    Remove use of refresh

--- test/elastic_record/integration/active_record_test.rb
+++ test/elastic_record/integration/active_record_test.rb
@@ -4,7 +4,6 @@ class ElasticRecord::ActiveRecordTest < MiniTest::Test
   def test_ordering
     poo_product = Project.create! name: "Poo"
     bear_product = Project.create! name: "Bear"
-    Project.elastic_index.refresh
 
     assert_equal [bear_product, poo_product], Project.elastic_relation.order(name: 'asc')
     assert_equal [poo_product, bear_product], Project.elastic_relation.order(name: 'desc')
@@ -12,9 +11,7 @@ class ElasticRecord::ActiveRecordTest < MiniTest::Test
 
   def test_update_callback
     project = Project.create! name: "Ideas"
-    Project.elastic_index.refresh
     project.update! name: 'Terrible Stuff'
-    Project.elastic_index.refresh
 
     assert_equal [project], Project.elastic_relation.filter(name: 'Terrible Stuff')
   end
