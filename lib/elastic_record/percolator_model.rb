2174e313f95a90eadb9d040bb0c6214bc06841f9
Kyle Unruh <kyle@gotime.com>
2018-04-05 17:39:00 -0600

    Add support for use against v5 indexes that already exist

--- lib/elastic_record/percolator_model.rb
+++ lib/elastic_record/percolator_model.rb
@@ -20,7 +20,11 @@ module ElasticRecord
       end
 
       def doctype
-        @doctype ||= Doctype.new(percolates_model.doctype.name, Doctype::PERCOLATOR_MAPPING)
+        @doctype ||=
+          begin
+            doctype_name = check_mapped_doctype_name || percolates_model.doctype.name
+            Doctype.new(doctype_name, Doctype::PERCOLATOR_MAPPING)
+          end
       end
 
       def percolate(document)
@@ -34,6 +38,26 @@ module ElasticRecord
 
         elastic_search.filter(query).limit(5000)
       end
+
+      private
+
+        def check_mapped_doctype_name
+          mapped_doctypes = elastic_connection.json_get("/#{base_class.name.demodulize.underscore.pluralize}/_mapping").values.first['mappings']
+
+          if mapped_doctypes.keys.count > 1
+            mapped_percolator_doctype(mapped_doctypes)
+          end
+        rescue ElasticRecord::ConnectionError
+          nil
+        end
+
+        def mapped_percolator_doctype(doctypes_map)
+          doctypes_map.each_pair do |doctype_name, mapping|
+            mapping['properties'].each_value do |value|
+              return doctype_name if value == {"type" => "percolator"}
+            end
+          end
+        end
     end
   end
 end
