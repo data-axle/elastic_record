2832fa29332cd47f776d1f7a21c3c402357d4e3e
Matthew Higgins <developer@matthewhiggins.com>
2018-04-25 21:53:21 -0700

    cleanups

--- lib/elastic_record/aggregation_response/aggregation.rb
+++ lib/elastic_record/aggregation_response/aggregation.rb
@@ -1,14 +1,14 @@
 module ElasticRecord
   module AggregationResponse
     class Aggregation
-      attr_accessor :name, :results
+      attr_accessor :name, :results, :meta
       def initialize(name, results)
         @name     = name
         @results  = results
-      end
 
-      def meta
-        results['meta']
+        @results.each do |key, value|
+          send("#{key}=", value) if respond_to?("#{key}=")
+        end
       end
 
       def inspect
--- lib/elastic_record/aggregation_response/single_bucket_aggregation.rb
+++ lib/elastic_record/aggregation_response/single_bucket_aggregation.rb
@@ -2,10 +2,7 @@ module ElasticRecord
   module AggregationResponse
     class SingleBucketAggregation < Aggregation
       include HasAggregations
-
-      def doc_count
-        results['doc_count']
-      end
+      attr_accessor :doc_count
     end
   end
 end
