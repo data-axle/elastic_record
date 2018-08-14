162114fdc48bf98ae398880e468e93ff61c468ce
Eric Barendt <ebarendt@users.noreply.github.com>
2018-08-14 15:50:46 -0700

    Expose methods for getting the raw results of a query (#78)

--- test/elastic_record/relation/hits_test.rb
+++ test/elastic_record/relation/hits_test.rb
@@ -1,10 +1,6 @@
 require 'helper'
 
 class ElasticRecord::Relation::HitsTest < MiniTest::Test
-  def test_to_hits
-    # assert Widget.elastic_relation.search_results.is_a?(ElasticSearch::Api::Hits)
-  end
-
   def test_to_ids
     red_widget = Widget.create(color: 'red')
     blue_widget = Widget.create(color: 'red')
@@ -33,5 +29,21 @@ class ElasticRecord::Relation::HitsTest < MiniTest::Test
     names = array.map(&:name)
     assert_includes names, 'Latte'
     assert_includes names, 'Americano'
+  end
+
+  def test_search_hits
+    coffees = [Project.new(name: 'Latte'), Project.new(name: 'Americano')]
+    Project.elastic_index.bulk_add(coffees)
+
+    array = Project.elastic_relation.search_hits
+    assert_equal %w(Latte Americano).to_set, array.map { |hit| hit["_source"]["name"] }.to_set
+  end
+
+  def test_search_results
+    coffees = [Project.new(name: 'Latte'), Project.new(name: 'Americano')]
+    Project.elastic_index.bulk_add(coffees)
+
+    results = Project.elastic_relation.search_results
+    %w(took timed_out _shards hits).each { |key| assert results.key?(key) }
   end
 end
