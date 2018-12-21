b047b92afb2fe233993fc465fd4f0e77346b8313
Eric Barendt <ericb@gotime.com>
2018-12-21 09:50:05 -0800

    Helper method for saving to ES

--- test/elastic_record/model_test.rb
+++ test/elastic_record/model_test.rb
@@ -24,4 +24,11 @@ class ElasticRecord::ModelTest < MiniTest::Test
     assert_equal InheritedModel, InheritedModel.elastic_index.model
     assert_equal 'widget', InheritedModel.elastic_index.mapping_type
   end
+
+  def test_save_to_elasticsearch
+    project = Project.new(name: 'scorpio')
+    project.save_to_elasticsearch
+
+    assert_equal [project.name], Project.filter(name: 'scorpio').map(&:name)
+  end
 end
