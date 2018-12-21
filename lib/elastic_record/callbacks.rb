03cf1efcc9182912eb9eefd4e48c67b587dad357
Eric Barendt <ericb@gotime.com>
2018-12-21 10:02:29 -0800

    Remove duplicate code

--- lib/elastic_record/callbacks.rb
+++ lib/elastic_record/callbacks.rb
@@ -4,17 +4,13 @@ module ElasticRecord
       return unless base.respond_to?(:after_save) &&  base.respond_to?(:after_destroy)
 
       base.class_eval do
-        after_create :create_index_document
+        after_create :save_to_elasticsearch
         after_update :update_index_document, if: -> { respond_to?(:saved_changes?) ? saved_changes? : changed? }
         after_destroy :delete_index_document
       end
     end
 
     private
-
-      def create_index_document
-        self.class.elastic_index.index_record self
-      end
 
       def update_index_document
         method = self.class.elastic_index.partial_updates ? :update_record : :index_record
