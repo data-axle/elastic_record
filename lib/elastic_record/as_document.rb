d29d7b6d862fcc21047cefd1dcadadb7e2a5668b
Austen Madden <austen@data-axle.com>
2019-05-29 15:12:33 -0400

    Alter flow to understand update vs search
    The previous fix was using ActiveRecord to determine behavior. Instead,
    refactoring the code to understand if it's an update vs a simple search
    seems like a better way to ensure we're doing the right thing. With this
    change, we'll still be setting the nil key if it's an update as it will
    be part of the changes, but we don't need to use provided? (nor mess
    with as_search_document)

--- lib/elastic_record/as_document.rb
+++ lib/elastic_record/as_document.rb
@@ -4,48 +4,42 @@ module ElasticRecord
       mapping_properties.each_with_object({}) do |(field, mapping), result|
         value = value_for_elastic_search field, mapping, mapping_properties
 
-        if !value.nil? || (respond_to?(:provided?) && provided?(field))
+        unless value.nil?
           result[field] = value
         end
       end
     end
 
-    def as_partial_update_document
-      mapping_properties = elastic_index.mapping[:properties]
+    def as_partial_update_document(mapping_properties = elastic_index.mapping[:properties])
       changed_fields = respond_to?(:saved_changes) ? saved_changes.keys : changed
 
       changed_fields.each_with_object({}) do |field, result|
         if field_mapping = mapping_properties[field]
-          result[field] = value_for_elastic_search field, field_mapping, mapping_properties
+          result[field] = value_for_elastic_search field, field_mapping, mapping_properties, true
         end
       end
     end
 
-    def value_for_elastic_search(field, mapping, mapping_properties)
-      value = try field
-      return if value.nil?
+    def value_for_elastic_search(field, mapping, mapping_properties, is_update = false)
+      return unless (value = try(field))
 
-      value =
-        case mapping[:type]&.to_sym
-        when :object
-          object_mapping_properties = mapping_properties.dig(field, :properties)
-          value_for_elastic_search_object(value, object_mapping_properties)
-        when :nested
-          object_mapping_properties = mapping_properties.dig(field, :properties)
-          value.map { |entry| value_for_elastic_search_object(entry, object_mapping_properties) }
-        when :integer_range, :float_range, :long_range, :double_range, :date_range
-          value_for_elastic_search_range(value)
-        else
-          value
-        end
-
-      if value.present? || value == false
-        value
+      case mapping[:type]&.to_sym
+      when :object
+        object_mapping_properties = mapping_properties.dig(field, :properties)
+        value_for_elastic_search_object(value, object_mapping_properties, is_update)
+      when :nested
+        object_mapping_properties = mapping_properties.dig(field, :properties)
+        value.map { |entry| value_for_elastic_search_object(entry, object_mapping_properties, is_update) }
+      when :integer_range, :float_range, :long_range, :double_range, :date_range
+        value_for_elastic_search_range(value)
+      else
+        value if value.present? || value == false
       end
     end
 
-    def value_for_elastic_search_object(object, nested_mapping)
-      object.respond_to?(:as_search_document) ? object.as_search_document(nested_mapping) : object
+    def value_for_elastic_search_object(object, nested_mapping, is_update)
+      method = is_update ? :as_partial_update_document : :as_search_document
+      object.respond_to?(method) ? object.public_send(method, nested_mapping) : object
     end
 
     def value_for_elastic_search_range(range)
