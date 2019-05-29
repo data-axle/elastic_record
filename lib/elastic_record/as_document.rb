0d378d7e9e406a94354074cb1b0ee8129ce8eba2
Austen Madden <austen@data-axle.com>
2019-05-29 11:15:53 -0400

    Handle objects that don't implement provided?

--- lib/elastic_record/as_document.rb
+++ lib/elastic_record/as_document.rb
@@ -4,7 +4,7 @@ module ElasticRecord
       mapping_properties.each_with_object({}) do |(field, mapping), result|
         value = value_for_elastic_search field, mapping, mapping_properties
 
-        if !value.nil? || provided?(field)
+        if !value.nil? || (respond_to?(:provided?) && provided?(field))
           result[field] = value
         end
       end
