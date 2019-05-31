5bae6bee762e2ecd3ed70dd829b009687c6ed466
Austen Madden <austen@data-axle.com>
2019-05-31 15:48:04 -0400

    Ensure provided nested object fields submit (#99)
    * Ensure provided nested object fields submit
    
    Currently in elastic record attempts to clear nested fields (set them
    to null) are dropped. To allow this, we need to refactor the code to
    understand if it's an update vs a simple search. With this change,
    we'll still be setting the nil key for updates.

--- lib/elastic_record/as_document.rb
+++ lib/elastic_record/as_document.rb
@@ -10,42 +10,38 @@ module ElasticRecord
       end
     end
 
-    def as_partial_update_document
-      mapping_properties = elastic_index.mapping[:properties]
+    def as_partial_update_document(mapping_properties = elastic_index.mapping[:properties])
       changed_fields = respond_to?(:saved_changes) ? saved_changes.keys : changed
 
       changed_fields.each_with_object({}) do |field, result|
         if field_mapping = mapping_properties[field]
-          result[field] = value_for_elastic_search field, field_mapping, mapping_properties
+          result[field] = value_for_elastic_search field, field_mapping, mapping_properties, true
         end
       end
     end
 
-    def value_for_elastic_search(field, mapping, mapping_properties)
-      value = try field
-      return if value.nil?
+    def value_for_elastic_search(field, mapping, mapping_properties, is_update = false)
+      return if (value = try(field)).nil?
 
-      value =
-        case mapping[:type]&.to_sym
-        when :object
-          object_mapping_properties = mapping_properties.dig(field, :properties)
-          value_for_elastic_search_object(value, object_mapping_properties)
-        when :nested
-          object_mapping_properties = mapping_properties.dig(field, :properties)
-          value.map { |entry| value_for_elastic_search_object(entry, object_mapping_properties) }
-        when :integer_range, :float_range, :long_range, :double_range, :date_range
-          value_for_elastic_search_range(value)
-        else
-          value
-        end
+      case mapping[:type]&.to_sym
+      when :object
+        object_mapping_properties = mapping_properties.dig(field, :properties)
+        value_for_elastic_search_object(value, object_mapping_properties, is_update)
+      when :nested
+        return nil if value.empty?
 
-      if value.present? || value == false
-        value
+        object_mapping_properties = mapping_properties.dig(field, :properties)
+        value.map { |entry| value_for_elastic_search_object(entry, object_mapping_properties, is_update) }
+      when :integer_range, :float_range, :long_range, :double_range, :date_range
+        value_for_elastic_search_range(value)
+      else
+        value if value.present? || value == false
       end
     end
 
-    def value_for_elastic_search_object(object, nested_mapping)
-      object.respond_to?(:as_search_document) ? object.as_search_document(nested_mapping) : object
+    def value_for_elastic_search_object(object, nested_mapping, is_update)
+      method = is_update ? :as_partial_update_document : :as_search_document
+      object.respond_to?(method) ? object.public_send(method, nested_mapping) : object
     end
 
     def value_for_elastic_search_range(range)
