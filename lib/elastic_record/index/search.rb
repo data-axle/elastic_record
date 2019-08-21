ed9f24e96b78e91e49b5db0e43ea3d30a2ec082c
Eric Barendt <ericb@gotime.com>
2019-08-21 14:17:36 -0700

    Accept options for msearch

--- lib/elastic_record/index/search.rb
+++ lib/elastic_record/index/search.rb
@@ -70,10 +70,13 @@ module ElasticRecord
         get url, elastic_query
       end
 
-      def multi_search(headers_and_bodies)
+      def multi_search(headers_and_bodies, options = {})
+        url = "_search"
+        url += "?#{options.to_query}" if options.any?
+
         queries = headers_and_bodies.flat_map { |header, body| [header.to_json, body.to_json] }
         queries = queries.join("\n") + "\n"
-        get "_msearch", queries
+        get url, queries
       end
 
       def explain(id, elastic_query)
