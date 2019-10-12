4e96b2e932e94f2189fbca6b6227c740e867f071
Austen Madden <austen@data-axle.com>
2019-10-12 11:37:36 -0400

    Fix test name
    Name of the test was incorrect. Also makes some style adjustments.

--- test/elastic_record/index/analyze_test.rb
+++ test/elastic_record/index/analyze_test.rb
@@ -1,12 +1,12 @@
 require 'helper'
 
-class ElasticRecord::Index::PercolatorTest < MiniTest::Test
+class ElasticRecord::Index::AnalyzeTest < MiniTest::Test
   def test_analyze
     tokens = Widget.elastic_index.analyze(
-      "analyzer" => "standard",
-      "text"     => "this is a test"
+      'analyzer' => 'standard',
+      'text'     => 'this is a test'
     )
 
-    assert_equal ["this", "is", "a", "test"], tokens
+    assert_equal %w[this is a test], tokens
   end
 end
