272416ec6b00297d806a76352550a319c191270f
Matthew Hinea <matthew.hinea@gmail.com>
2022-05-17 20:44:58 -0700

    Improve error messaging around expired PITs

--- lib/elastic_record/index/pagination.rb
+++ lib/elastic_record/index/pagination.rb
@@ -28,6 +28,12 @@ module ElasticRecord
         else
           get '_search', elastic_query
         end
+      rescue ElasticRecord::ConnectionError => e
+        case e.status_code
+        when '400' then raise ElasticRecord::InvalidPointInTimeError, e.message
+        when '404' then raise ElasticRecord::ExpiredPointInTime, e.message
+        else raise e
+        end
       end
 
       private
