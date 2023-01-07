aef3c422e8a603d8702c4352c616340b8d890c7a
Matthew Hinea <matthew.hinea@gmail.com>
2023-01-06 21:14:18 -0800

    Specify what error is being tested in test names

--- test/elastic_record/errors_test.rb
+++ test/elastic_record/errors_test.rb
@@ -1,13 +1,13 @@
 require 'helper'
 
 class ElasticRecord::ErrorsTest < MiniTest::Test
-  def test_with_a_payload
+  def test_connection_error_with_a_payload
     response_body   = '{"foo": "bar"}'
     request_payload = '{"fiz": "buz"}'
     ElasticRecord::ConnectionError.new(200, response_body, request_payload)
   end
 
-  def test_without_a_payload
+  def test_connection_error_without_a_payload
     response_body = '{"foo": "bar"}'
     ElasticRecord::ConnectionError.new(200, response_body)
   end
