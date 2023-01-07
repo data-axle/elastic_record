e9a2e466ebfa73f76098afc63381b066b1cb42c8
Matthew Hinea <matthew.hinea@gmail.com>
2023-01-06 21:12:12 -0800

    Combine payload as valid JSON

--- lib/elastic_record/errors.rb
+++ lib/elastic_record/errors.rb
@@ -4,11 +4,21 @@ module ElasticRecord
 
   class ConnectionError < Error
     attr_reader :status_code, :payload
+
     def initialize(status_code, message, payload = nil)
       @status_code = status_code
-      message = "#{message} (for payload '#{payload}')" if payload
-      super(message)
+      super(combine(message, payload))
     end
+
+    private
+
+      def combine(message, payload)
+        JSON.parse(message).merge(
+          {
+            'elastic_record_payload' => JSON.parse(payload || '{}')
+          }
+        ).to_json
+      end
   end
 
   class BulkError < Error
---
+++ test/elastic_record/errors_test.rb
@@ -0,0 +1,14 @@
+require 'helper'
+
+class ElasticRecord::ErrorsTest < MiniTest::Test
+  def test_with_a_payload
+    response_body   = '{"foo": "bar"}'
+    request_payload = '{"fiz": "buz"}'
+    ElasticRecord::ConnectionError.new(200, response_body, request_payload)
+  end
+
+  def test_without_a_payload
+    response_body = '{"foo": "bar"}'
+    ElasticRecord::ConnectionError.new(200, response_body)
+  end
+end
