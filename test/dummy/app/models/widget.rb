1112b7d9e71a080a8004e2d3fd78c4c7b8542366
Matthew Hinea <matthew.hinea@gmail.com>
2024-01-23 10:37:11 -0800

    Make test model `belongs_to` optional: true

--- test/dummy/app/models/widget.rb
+++ test/dummy/app/models/widget.rb
@@ -2,7 +2,7 @@ class Widget < ActiveRecord::Base
   include ElasticRecord::Model
   self.elastic_index.partial_updates = true
 
-  belongs_to :warehouse
+  belongs_to :warehouse, optional: true
   validates :color, format: {with: /[a-z]/}
 
   class WidgetPart
