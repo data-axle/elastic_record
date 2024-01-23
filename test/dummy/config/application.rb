81be71fe032e4db6d56d70daf2f4f762ebd2d83f
Matthew Hinea <matthew.hinea@gmail.com>
2024-01-23 12:01:59 -0800

    Clean up application.rb - just move a line

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -2,9 +2,10 @@ require_relative "boot"
 
 require "rails/all"
 
-Bundler.require(*Rails.groups)
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
 require "elastic_record"
 
 module Dummy
