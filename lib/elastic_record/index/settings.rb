f7f4d3e6d7a6289be4d7778a6d0093f458a3d58d
Matthew Hinea <matthew.hinea@gmail.com>
2025-08-04 13:07:03 -0700

    Define a helper for toggling write blocks

--- lib/elastic_record/index/settings.rb
+++ lib/elastic_record/index/settings.rb
@@ -12,6 +12,10 @@ module ElasticRecord
       def update_settings(index_name = alias_name, settings: self.settings)
         connection.json_put "/#{index_name}/_settings", settings
       end
+
+      def toggle_write_block(value)
+        update_settings(settings: { index: { 'blocks.write': value } })
+      end
     end
   end
 end
