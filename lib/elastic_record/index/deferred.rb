95f3a4576e57f6671f7ceb6cf6cc0449a2d96096
Matthew Hinea <matthew.hinea@gmail.com>
2026-09-21 17:37:41 -0700

    make deferred connection use real bulk actions

--- lib/elastic_record/index/deferred.rb
+++ lib/elastic_record/index/deferred.rb
@@ -11,12 +11,18 @@ module ElasticRecord
         attr_accessor :index
         attr_accessor :deferred_actions
         attr_accessor :writes_made
-        attr_accessor :bulk_actions
 
         def initialize(index)
           @index = index
-          @bulk_actions = nil
           reset!
+        end
+
+        def bulk_actions
+          index.real_connection.bulk_actions
+        end
+
+        def bulk_actions=(actions)
+          index.real_connection.bulk_actions = actions
         end
 
         def reset!
